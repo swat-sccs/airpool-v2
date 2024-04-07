206f3d602aca291761c4b6136e9f12e53659b2e3
Finn McKibbin <fmckibb1@swarthmore.edu>
2024-04-07 19:09:03 -0400

    Implemented Text Box Component for the Creation Page

--- app/temp-test/page.tsx
+++ app/temp-test/page.tsx
@@ -5,6 +5,7 @@ import SCCSHeader from '@/components/SCCSHeader'
 import Link from 'next/link'
 import CalendarView from '@/components/CalendarView\''
 import AirpoolDropdown from '@/components/AirpoolDropdown'
+import CreateFormTextBox from '@/components/CreateFormTextBox'
 
 export default function Home() {
 
@@ -21,9 +22,8 @@ export default function Home() {
   return (
     <>
         <div className='mt-[10px]'>
-            <AirpoolDropdown id={"test"} 
-            options={[["Swarthmore", "swarthmore"], ["Bryn Mawr", "bryn-mawr"], ["Haverford", "haverford"], ["Custom", "custom"]]}
-            widthClass={"w-[260px]"}/>
+            <CreateFormTextBox title={"Meeting Directions"} smallText = {"This is some test small text"} placeholder={"Please enter your text here..."} 
+              bigTextClass={"text-[42px]"} width={600} height={200}/>
         </div>
     </>
   )
---
+++ components/CreateFormTextBox.tsx
@@ -0,0 +1,27 @@
+
+/**
+ * 
+ * Props:
+ *   title (string): The title/header/label describing what to put in the textbox
+ *   smallText (string): The small text that appears below the textbox giving further context/prompting (parens are automatically included)
+ *   titleClass (string of tailwind classes): Tailwind classes to apply to the title
+ *   smallTextClass (string of tailwind classes): Tailwind classes to apply to the smallTest
+ *   placeholder (string): Text/prompt to display in the textbox when it is empty (i.e. "Type your text here...")
+ *   width (int): The width of textbox (default pixels)
+ *   height (int): The height of textbook (default pixels)
+ *   [TO GIA: Don't worry yet about prpviding values for these fields below]
+ *   name (string): Name for the contents of the text field (for use in forms)
+ *   id (string): ID for the contents of the text field (for use in forms)
+ * 
+ */
+export default function CreateFormTextBox(props: any){
+    return (
+        <div className="flex flex-col items-center">
+            <div className={props.bigTextClass + " mb-[18px] self-start"}>{props.title}:</div>
+            <textarea name={props.name} id={props.id} placeholder={props.placeholder} 
+                style={{width: props.width, height: props.height,}}
+                className="bg-accent rounded-slight drop-shadow-dropdown outline-none placeholder:italic p-[17px_13px] text-[24px]"/>
+            <div className={props.smallTextClass + " mt-[21px]"}>({props.smallText})</div>
+        </div>
+    )
+}
