16592f895d2ff06b10152216bba8e01c3cee092a
Yaejie Lee Kwon <yaejiekwon@dhcp-130-58-100-120.swarthmore.edu>
2024-04-28 17:42:10 -0400

    create pool page integration

--- app/create-pools/page.tsx
+++ app/create-pools/page.tsx
@@ -8,6 +8,7 @@ import SmallTitle from "@/components/SmallTitle";
 import SmallText from "@/components/SmallText";
 import DateDropdown from "@/components/DateDropdown";
 import CreatePoolButton from "@/components/CreatePoolButton";
+import SingleDropdown from "@/components/SingleDropdown";
 
 export default function Create_Pools() {
 
@@ -17,8 +18,37 @@ export default function Create_Pools() {
         <div className="mt-[64px]">
             <SCCSBox contents={"Create a Carpool"} extraClasses={"text-[50px] w-[88vw]"}></SCCSBox>
         </div>
-        <div className="mt-[55px] w-[71vw] bg-card-bg rounded m-auto drop-shadow flex flex-col items-center">
+        <div className="mt-[95px] w-[71vw] bg-card-bg rounded m-auto drop-shadow flex flex-col items-center">
             <Instructions Description={"Please Fill Out the Form Below"} ></Instructions>
+            <div className="mt-[75px] flex flex-row items-center">
+                <SmallTitle Description="Destination:"></SmallTitle> 
+                <AirpoolDropdown options={["Philadelphia Airport", "Haverford", "Custom"]} width="552px"> </AirpoolDropdown>
+            </div>
+            <div className="mt-[75px] flex flex-row items-center">
+                <SmallTitle Description="Meeting Place:"></SmallTitle> 
+                <AirpoolDropdown options={["Parrish Hall", "Whittier Circle", "Custom"]} width="552px"> </AirpoolDropdown>
+            </div>
+            <div className="mt-[75px] flex flex-row items-center">
+                <SmallTitle Description="Date:"></SmallTitle> 
+                <AirpoolDropdown options={["January", "February", "March", "April", "May", "June"]} width="200px"> </AirpoolDropdown>
+                <AirpoolDropdown options={["1", "2", "3", "4", "5", "31"]} width="100px"> </AirpoolDropdown>
+                <AirpoolDropdown options={["2024", "2023", "2022"]} width="100px"> </AirpoolDropdown>
+            </div>
+            <div className="mt-[20px] flex flex-row items-center">
+                <SmallTitle Description="Time:"></SmallTitle> 
+                <AirpoolDropdown options={["1", "2", "3", "4", "5", "12"]} width="100px"> </AirpoolDropdown>
+                <AirpoolDropdown options={["1", "2", "3", "4", "5", "60"]} width="100px"> </AirpoolDropdown>
+                <AirpoolDropdown options={["AM", "PM"]} width="100px"> </AirpoolDropdown>
+            </div>
+            <div className="mt-[20px] flex flex-row items-center">
+                <SmallTitle Description="Seats Available:"></SmallTitle> 
+                <AirpoolDropdown options={["1", "2", "3", "4", "5", "6"]} width="100px"> </AirpoolDropdown>
+                <SmallText Description= "(How many passengers other than yourself can you accommodate?)"></SmallText>
+            </div>
+            <div className="mt-[20-px] mb-[75px] flex flex-row items-center">
+                <SmallTitle Description="Vehicle Type:"></SmallTitle> 
+                <AirpoolDropdown options={["Uber", "Lyft"]} width="200px"> </AirpoolDropdown>
+            </div>
             <SmallTitle Description="Payment Methods" ></SmallTitle>
             <SmallText Description="Select all payment methods that you would accept from fellow passengers"></SmallText>
             <div className="gap-[5vw] flex flex-row">
--- components/CreateFormTextBox.tsx
+++ components/CreateFormTextBox.tsx
@@ -21,7 +21,7 @@ export default function CreateFormTextBox(props: any){
             <textarea name={props.name} id={props.id} placeholder={props.placeholder} 
                 style={{width: props.width, height: props.height,}}
                 className="bg-accent rounded-slight drop-shadow-dropdown outline-none placeholder:italic p-[17px_13px] text-[24px]"/>
-            <div className={props.smallTextClass + "mt-[21px] text-[22px] text-gray-500"}>{props.smallText}</div>
+            <div className={props.smallTextClass + "mt-[21px] ml-[30px] text-[22px] text-gray-500"}>{props.smallText}</div>
         </div>
     )
 }
