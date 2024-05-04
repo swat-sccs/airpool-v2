7dfabe76e6154f0be8bded7a8d64fe0f3baf7af1
Dylan <djacobs2@swarthmore.edu>
2024-05-03 23:21:36 -0400

    search pools updated

--- app/search-pools/page.tsx
+++ app/search-pools/page.tsx
@@ -1,102 +1,20 @@
-"use client"
-import CalendarView from "@/components/CalendarView'";
 import CarpoolCard from "@/components/CarpoolCard";
-import SCCSBox from "@/components/SCCSBox";
-import { useState } from "react";
-export const dynamic = 'force-dynamic' // defaults to auto
+import Page from "./search-pools-page"
 import { PrismaClient, TransportationType} from '@prisma/client'
 
-// This is completely temporary and just for testing until we get the hecking backend setup
-const poolData = [
-    {
-        "isDotBlue": true,
-        "destination": "Philly Airport",
-        "vehicleType": "UberX",
-        "paymentMethods": ["Cash", "Venmo"],
-        "seatCount": 4,
-        "isRoundTrip": false,
-        "time": "1:00 AM",
-        "date": "April 1, 2023"
-    },
-    {
-        "isDotBlue": false,
-        "destination": "SCCS Lab",
-        "vehicleType": "UberX",
-        "paymentMethods": ["Apple Pay", "Cash", "Venmo", "Zelle"],
-        "seatCount": 61,
-        "isRoundTrip": false,
-        "time": "2:30 PM",
-        "date": "April 6, 2023"
-    },
-    {
-        "isDotBlue": false,
-        "destination": "Walmart",
-        "vehicleType": "Lyft",
-        "paymentMethods": ["Apple Pay"],
-        "seatCount": 1,
-        "isRoundTrip": false,
-        "time": "11:00 AM",
-        "date": "April 4, 2023"
-    },
-    {
-        "isDotBlue": true,
-        "destination": "Haverford",
-        "vehicleType": "Car",
-        "paymentMethods": ["Cash"],
-        "seatCount": 3,
-        "isRoundTrip": false,
-        "time": "3:45 PM",
-        "date": "April 10, 2023"
-    },
-    {
-        "isDotBlue": false,
-        "destination": "Mushroom Kingdom",
-        "vehicleType": "Yoshi",
-        "paymentMethods": ["Apple Pay", "Venmo", "Zelle"],
-        "seatCount": 4,
-        "isRoundTrip": false,
-        "time": "11:11 PM",
-        "date": "April 11, 2023"
-    },
-]
 
-export async function GetAllPools() {
+export default async function SearchPoolsPage(){
     const prisma = new PrismaClient({});
-    return await prisma.carpool.findMany();
-}
-
-const displayTypes = ["list", "grid"];
-
-export default async function Pools(){
-
-    const [displayTypeIndex, setDisplayTypeIndex] = useState(0);
-
-    function toggleDisplayType(){
-        setDisplayTypeIndex((displayTypeIndex + 1) % displayTypes.length);
-    }
-
-    const pools = await GetAllPools(); // i think this needs to be removed, or else it will cause the page to crash
-
+    const pools = await prisma.carpool.findMany();
+    const data = pools.map(function(p){
+        var paymentTypes = [""];
+        if (p.acceptsApplePay) {paymentTypes.push("Apple Pay")}
+        if (p.acceptsCash) {paymentTypes.push("Cash")}
+        if (p.acceptsVenmo) {paymentTypes.push("Venmo")}
+        if (p.acceptsZelle) {paymentTypes.push("Zelle")}
+        return <CarpoolCard isDotBlue={true} destination={p.destination} vehicleType={p.transportationType.toString()} paymentMethods={paymentTypes} seatCount={p.availableSeats} time={p.meetingTime.toLocaleString()} date={p.meetingTime.toLocaleString()} displayType={displayTypes[displayTypeIndex]} />
+    })
     return (
-        <>
-            <button onClick={toggleDisplayType}>Toggle Display Type</button>
-            <div className="mt-[25px]">
-                <SCCSBox extraClasses="w-[88vw]" contents={"Search Pools"} />
-            </div>
-                        
-            <div className={`flex flex-wrap items-center ${displayTypeIndex == 0 ? "flex-col" : "flex-row gap-[80px]"} mt-[40px]`}>
-            {
-            
-                pools.map(function(p){
-                    var paymentTypes = [""];
-                    if (p.acceptsApplePay) {paymentTypes.push("Apple Pay")}
-                    if (p.acceptsCash) {paymentTypes.push("Cash")}
-                    if (p.acceptsVenmo) {paymentTypes.push("Venmo")}
-                    if (p.acceptsZelle) {paymentTypes.push("Zelle")}
-                    return <CarpoolCard isDotBlue={true} destination={p.destination} vehicleType={p.transportationType.toString()} paymentMethods={paymentTypes} seatCount={p.availableSeats} time={p.meetingTime.toLocaleString()} date={p.meetingTime.toLocaleString()} displayType={displayTypes[displayTypeIndex]} />
-                })
-            }
-            </div>
-        </>
+        <Page content={data}/> 
     )
 }
