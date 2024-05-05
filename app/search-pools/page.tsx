440491e44843ca71bca32b3a91bbf6191ac78866
Dylan <djacobs2@swarthmore.edu>
2024-05-05 15:06:29 -0400

    fixed more search-pools bugs

--- app/search-pools/page.tsx
+++ app/search-pools/page.tsx
@@ -6,18 +6,9 @@ import { PrismaClient, TransportationType} from '@prisma/client'
 
 export default async function SearchPoolsPage(){
     
-    const displayType  = "list"; // TODO: make this dynamic
     const prisma = new PrismaClient({});
     const pools = await prisma.carpool.findMany();
-    const data = pools.map(function(p){
-        var paymentTypes = [""];
-        if (p.acceptsApplePay) {paymentTypes.push("Apple Pay")}
-        if (p.acceptsCash) {paymentTypes.push("Cash")}
-        if (p.acceptsVenmo) {paymentTypes.push("Venmo")}
-        if (p.acceptsZelle) {paymentTypes.push("Zelle")}
-        return <CarpoolCard isDotBlue={true} destination={p.destination} vehicleType={p.transportationType.toString()} paymentMethods={paymentTypes} seatCount={p.availableSeats} time={p.meetingTime.toLocaleString()} date={p.meetingTime.toLocaleString()} displayType={displayType} />
-    })
     return (
-        <Page content={data}/> 
+        <Page carpools={pools}/> 
     )
 }
