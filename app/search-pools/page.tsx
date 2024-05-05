1decf0b549cd1dc4ed84452a5c972b314b6221fb
Dylan <djacobs2@swarthmore.edu>
2024-05-05 10:32:24 -0400

    search pools page fixed

--- app/search-pools/page.tsx
+++ app/search-pools/page.tsx
@@ -1,9 +1,12 @@
 import CarpoolCard from "@/components/CarpoolCard";
 import Page from "./search-pools-page"
+import Pools  from "./search-pools-page";
 import { PrismaClient, TransportationType} from '@prisma/client'
 
 
 export default async function SearchPoolsPage(){
+    
+    const displayType  = "list"; // TODO: make this dynamic
     const prisma = new PrismaClient({});
     const pools = await prisma.carpool.findMany();
     const data = pools.map(function(p){
@@ -12,7 +15,7 @@ export default async function SearchPoolsPage(){
         if (p.acceptsCash) {paymentTypes.push("Cash")}
         if (p.acceptsVenmo) {paymentTypes.push("Venmo")}
         if (p.acceptsZelle) {paymentTypes.push("Zelle")}
-        return <CarpoolCard isDotBlue={true} destination={p.destination} vehicleType={p.transportationType.toString()} paymentMethods={paymentTypes} seatCount={p.availableSeats} time={p.meetingTime.toLocaleString()} date={p.meetingTime.toLocaleString()} displayType={displayTypes[displayTypeIndex]} />
+        return <CarpoolCard isDotBlue={true} destination={p.destination} vehicleType={p.transportationType.toString()} paymentMethods={paymentTypes} seatCount={p.availableSeats} time={p.meetingTime.toLocaleString()} date={p.meetingTime.toLocaleString()} displayType={displayType} />
     })
     return (
         <Page content={data}/> 
